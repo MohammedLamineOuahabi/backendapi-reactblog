cd8a7e1cee560c6aae3d222153e5a52483e1e028
amine <amine@adress.com>
2020-08-10 19:06:07 +0100

    backend api for react blog

---
+++ db.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongodb = require("mongodb");
+
+mongodb.connect(
+  process.env.CONNECTIONSTRING,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, client) {
+    if (err) {
+      console.log(err);
+    }
+
+    module.exports = client.db();
+    const app = require("./app");
+    app.listen(process.env.PORT);
+  }
+);
